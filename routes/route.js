c6d80e751995874fde271bb7eecceb6e836be8a3
yulisetyobudi <yuli.setyo@gmail.com>
2019-04-11 11:59:37 +0700

    perbaikan routes/route.js

--- routes/route.js
+++ routes/route.js
@@ -2,30 +2,32 @@ var express = require('express');
 var router = express.Router();
 var model = require('../model/modeluser');
 var controller = require('../controllers/userController');
+var passport = require('../local');
 
 //definisi route
-router.post('/gembel', controller.login);
+router.post('/login', controller.login); 
+router.get('/', 
+    function(req, res){
+        res.send('Hello, this is my first express');
+    });
+router.get('/users', 
+    passport.authenticate('jwt', {session: false}),
+    function(req, res){
+        //dari koneksi mysql
+        model.getUsers(res);
+        //var model = require('./modeluser');
 
-router.get('/', function(req, res){
-    res.send('Hello, this is my first express');
-});
-
-router.get('/user', function(req, res){
-    //var model = require('./modeluser');
-    //dari file json
-    // res.json(model.getAllUser());
-
-    //dari koneksi mysql
-    model.getUsers(res);
-});
-
-router.get('/user/:id', function(req, res){
-    //var model = require('./modeluser');
-    model.getUser(req, res);
-});
-
-router.post('./user', function(req, res){
-    res.json(model.getAllUser());
-});
+        //dari file json
+        // res.json(model.getAllUser());
+    });
+router.get('/user/:id', 
+    function(req, res){
+        //var model = require('./modeluser');
+        model.getUser(req, res);
+    });
+router.post('./user', 
+    function(req, res){
+        res.json(model.getAllUser());
+    });
 
 module.exports = router;
