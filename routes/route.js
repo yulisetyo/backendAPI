7f1e4be13de5c3d565437394dff23a39509f003d
yulisetyobudi <yuli.setyo@gmail.com>
2019-04-11 14:27:54 +0700

    perbaikan kode

--- routes/route.js
+++ routes/route.js
@@ -4,12 +4,18 @@ var model = require('../model/modeluser');
 var controller = require('../controllers/userController');
 var passport = require('../local');
 
+exports.users = (req, res)=>{
+    User()
+}
+
 //definisi route
 router.post('/login', controller.login); 
 router.get('/', 
     function(req, res){
         res.send('Hello, this is my first express');
     });
+
+/*
 router.get('/users', 
     passport.authenticate('jwt', {session: false}),
     function(req, res){
@@ -20,14 +26,28 @@ router.get('/users',
         //dari file json
         // res.json(model.getAllUser());
     });
+*/
+router.get('/users', 
+    passport.authenticate('jwt', {session: false}),
+    controller.users);
+
+    /*
 router.get('/user/:id', 
     function(req, res){
         //var model = require('./modeluser');
         model.getUser(req, res);
     });
+*/
+router.get('/user/:id',controller.user)
+
+/*
 router.post('./user', 
     function(req, res){
         res.json(model.getAllUser());
     });
+*/
+
+router.post('/user', controller.add)
+
 
 module.exports = router;
